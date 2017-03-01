7a046e0809a44af98eefccef63653a1a2b2ceb7e
Adrian <adrpieper@gmail.com>
2017-03-01 18:41:45 +0100

    Dodałem listę

--- src/animal/AnimalTest.java
+++ src/animal/AnimalTest.java
@@ -1,9 +1,27 @@
 package animal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Adrian on 2017-03-01.
  */
 public class AnimalTest {
 
+    public static void main(String[] args) {
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+        Animal[] animals = new Animal[]{cat, dog};
+        for (Animal animal : animals) {
+            animal.makeVoise();
+        }
+        List<Animal> animalList = new ArrayList<>();
+        animalList.add(cat);
+        animalList.add(dog);
+        for (Animal animal : animalList) {
+            animal.makeVoise();
+        }
 
+
+    }
 }
