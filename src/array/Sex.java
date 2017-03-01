c980aa8b4b4b425989a7e0aacbaf375ed73a1d8a
Adrian <adrpieper@gmail.com>
2017-03-01 20:43:24 +0100

    Płeć Enum example

---
+++ src/array/Sex.java
@@ -0,0 +1,10 @@
+package array;
+
+/**
+ * Created by Adrian on 2017-03-01.
+ */
+public enum  Sex {
+    MALE,
+    FEMALE;
+
+}
---
+++ src/array/SexExample.java
@@ -0,0 +1,13 @@
+package array;
+
+/**
+ * Created by Adrian on 2017-03-01.
+ */
+public class SexExample {
+
+    public static void main(String[] args) {
+        System.out.println(Sex.FEMALE.name());
+        System.out.println(Sex.MALE.name());
+
+    }
+}
