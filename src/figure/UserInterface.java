ef56bf80745d80b19548f8174a6eeba3b42f4286
Adrian <adrpieper@gmail.com>
2017-03-01 19:29:18 +0100

    Dodałem interfest użytkownika

---
+++ src/figure/UserInterface.java
@@ -0,0 +1,29 @@
+package figure;
+
+import java.util.Scanner;
+
+/**
+ * Created by Adrian on 2017-03-01.
+ */
+public class UserInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Wybierz figurę:");
+        System.out.println("1 - kwadrat");
+        System.out.println("2 - koło");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        switch (i) {
+            case 1:
+                System.out.println("Podaj bok");
+                double a = scanner.nextDouble();
+                Square square = new Square(a);
+                System.out.println("Pole: " + square.countArea());
+                System.out.println("Obwód: " + square.countCircumference());
+                break;
+            case 2:
+
+                break;
+        }
+    }
+}
